ff0dc3c8bb5a698588de7c44845655f50f39150b
MicahMeadows <micahm213@gmail.com>
2022-08-19 15:02:12 -0400

    Fix swipe up selection box

--- sender/lib/widgets/pages/home/list_choice_dialog.dart
+++ sender/lib/widgets/pages/home/list_choice_dialog.dart
@@ -20,48 +20,46 @@ class ListChoiceDialog extends StatelessWidget {
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        IntrinsicHeight(
-          child: Container(
-            decoration: BoxDecoration(
-              color: col.primary,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(.61),
-                  blurRadius: 10,
-                  offset: const Offset(0, 4),
+        Container(
+          decoration: BoxDecoration(
+            color: col.primary,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(.61),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          margin: EdgeInsets.only(top: 32),
+          padding: EdgeInsets.all(20),
+          width: MediaQuery.of(context).size.width * .8,
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Text(
+                "Where should we put the route?",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'nunito',
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-            margin: EdgeInsets.only(top: 32),
-            padding: EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width * .8,
-            child: Column(
-              children: [
-                const SizedBox(height: 20),
-                const Text(
-                  "Where should we put the route?",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontFamily: 'nunito',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    for (var option in options)
-                      ThickButton(
-                        text: option.buttonText,
-                        onPressed: option.onTap,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                      ),
-                  ],
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  for (var option in options)
+                    ThickButton(
+                      text: option.buttonText,
+                      onPressed: option.onTap,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    ),
+                ],
+              ),
+            ],
           ),
         ),
         Positioned(
