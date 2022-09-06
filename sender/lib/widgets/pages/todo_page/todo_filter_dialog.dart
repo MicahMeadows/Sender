8756ee822ae90c68ac5f41e601a87e6f63261b9f
MicahMeadows <micahm213@gmail.com>
2022-09-05 23:39:46 -0400

    Add clear filter button

--- sender/lib/widgets/pages/todo_page/todo_filter_dialog.dart
+++ sender/lib/widgets/pages/todo_page/todo_filter_dialog.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sender/common/constants/colors.dart' as col;
@@ -6,6 +8,7 @@ import 'package:sender/data/models/tick_filters/tick_filters.dart';
 
 import '../../common/rating_widget.dart';
 import '../../common/thick_button.dart';
+import '../settings/settings_helper.dart';
 
 class TodoFilterDialog extends StatefulWidget {
   const TodoFilterDialog({Key? key}) : super(key: key);
@@ -18,8 +21,7 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
   late TickFilters pageFilters;
   late final _filterCubit = context.read<TickFilterCubit>();
 
-  @override
-  void initState() {
+  void initPageFilters() {
     _filterCubit.state.whenOrNull(
       set: (filters) {
         setState(() {
@@ -37,6 +39,12 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
         );
       },
     );
+  }
+
+  @override
+  void initState() {
+    initPageFilters();
+
     super.initState();
   }
 
@@ -51,9 +59,86 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
         ),
         child: Column(
           children: [
-            const Text('Set Filters'),
-            Text('Minimum Rating'),
             Row(
+              children: [
+                const Spacer(flex: 1),
+                const Text(
+                  'Set Filters',
+                  style: TextStyle(fontSize: 28),
+                ),
+                if (pageFilters.numFilters > 0)
+                  Expanded(
+                    flex: 1,
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          _filterCubit.clearFilters();
+                          initPageFilters();
+                        });
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Text(
+                          'clear',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: col.error,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                if (pageFilters.numFilters == 0)
+                  const Spacer(
+                    flex: 1,
+                  ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            const Text('Grade Range'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DropdownButton<String>(
+                    value: pageFilters.minGrade,
+                    focusColor: col.text1,
+                    dropdownColor: col.primary,
+                    items: gradeOptions
+                        .map((e) =>
+                            DropdownMenuItem<String>(value: e, child: Text(e)))
+                        .toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        final newMinGrade = val ?? '';
+                        pageFilters =
+                            pageFilters.copyWith(minGrade: newMinGrade);
+                      });
+                    }),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Text('to'),
+                ),
+                DropdownButton<String>(
+                    value: pageFilters.maxGrade,
+                    focusColor: col.text1,
+                    dropdownColor: col.primary,
+                    items: gradeOptions
+                        .map((e) =>
+                            DropdownMenuItem<String>(value: e, child: Text(e)))
+                        .toList(),
+                    onChanged: (val) {
+                      setState(() {
+                        final newMaxGrade = val ?? '';
+                        pageFilters =
+                            pageFilters.copyWith(maxGrade: newMaxGrade);
+                      });
+                    }),
+              ],
+            ),
+            const SizedBox(height: 15),
+            const Text('Minimum Rating'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 InkWell(
                   onTap: () {
@@ -69,10 +154,11 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
                 ),
                 const SizedBox(width: 5),
                 RatingWidget(
-                    color: col.accent,
-                    rating: pageFilters.minRating,
-                    numStarsShow: 3,
-                    height: 30),
+                  color: col.accent,
+                  rating: pageFilters.minRating,
+                  numStarsShow: 3,
+                  height: 35,
+                ),
                 const SizedBox(width: 5),
                 InkWell(
                   onTap: () {
@@ -88,7 +174,53 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
                 ),
               ],
             ),
+            const SizedBox(height: 15),
             Text('Climb Types'),
+            FittedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Trad'),
+                  Switch(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      value: pageFilters.enableTrad,
+                      onChanged: (newVal) {
+                        setState(() {
+                          pageFilters =
+                              pageFilters.copyWith(enableTrad: newVal);
+                        });
+                      }),
+                  Text('Sport'),
+                  Switch(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      value: pageFilters.enableSport,
+                      onChanged: (newVal) {
+                        setState(() {
+                          pageFilters =
+                              pageFilters.copyWith(enableSport: newVal);
+                        });
+                      }),
+                ],
+              ),
+            ),
+            FittedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Top Rope'),
+                  Switch(
+                      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      value: pageFilters.enableTopRope,
+                      onChanged: (newVal) {
+                        setState(() {
+                          pageFilters =
+                              pageFilters.copyWith(enableTopRope: newVal);
+                        });
+                      }),
+                ],
+              ),
+            ),
+            const SizedBox(height: 15),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               mainAxisSize: MainAxisSize.min,
@@ -105,7 +237,11 @@ class _TodoFilterDialogState extends State<TodoFilterDialog> {
                 ThickButton(
                   text: 'Save',
                   onPressed: () {
-                    _filterCubit.setFilters(pageFilters);
+                    if (pageFilters.numFilters == 0) {
+                      _filterCubit.clearFilters();
+                    } else {
+                      _filterCubit.setFilters(pageFilters);
+                    }
                     Navigator.of(context).pop(this);
                   },
                 ),
